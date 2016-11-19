818c980f377fb85ee1dc3193484fea4b20be1ff2
Bryan Shelton <bryan@sheltonplace.com>
2016-11-19 14:27:04 -0800

    HTTP to HTTPS redirect option

--- http.go
+++ http.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
+	"os"
 	"strings"
 	"time"
 )
@@ -15,6 +17,9 @@ type Server struct {
 }
 
 func (s *Server) ListenAndServe() {
+	if s.Opts.RedirectHttpToHttps {
+		go s.ServeHTTPSRedirector()
+	}
 	if s.Opts.TLSKeyFile != "" || s.Opts.TLSCertFile != "" {
 		s.ServeHTTPS()
 	} else {
@@ -89,6 +94,12 @@ func (s *Server) ServeHTTPS() {
 	}
 
 	log.Printf("HTTPS: closing %s", tlsListener.Addr())
+}
+
+func (s *Server) ServeHTTPSRedirector() {
+	h := LoggingHandler(os.Stdout, NewRedirectHandler(*s.Opts), s.Opts.RequestLogging)
+	log.Printf("HTTPs redirector listening on: %s", s.Opts.HttpsRedirectorAddress)
+	log.Fatal(http.ListenAndServe(s.Opts.HttpsRedirectorAddress, h))
 }
 
 // tcpKeepAliveListener sets TCP keep-alive timeouts on accepted
